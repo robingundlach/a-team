880f99d45d0b9b1576fdc799a96147eaa2d48cdf
Robin Gundlach <gundlachrobin@gmail.com>
2022-12-05 14:42:41 +0100

    verbesserung

--- Aufgabe3/insertSort/src/Main.java
+++ Aufgabe3/insertSort/src/Main.java
@@ -2,14 +2,15 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] unsortedArray = {8, 7, 5, 3, 1, 9, 6, 4, 2};
-        System.out.println(Arrays.toString(unsortedArray));
-        System.out.println(Arrays.toString(selectionSort(unsortedArray)));
+        int[] unsortedArray = {-1, 7, 5, 3, 1, 9, 6, 4, 2};
+        System.out.printf("UnsortedArray: %s%n",Arrays.toString(unsortedArray));
+        System.out.printf("SortedArray:   %s%n",Arrays.toString(selectionSort(unsortedArray)));
     }
     public static int[] selectionSort(int[] unsortedArray){
         for (int i = 0; i< unsortedArray.length -1; i++){
             for (int j = i+1; j < unsortedArray.length; j++) {
-                if (unsortedArray[j] < unsortedArray[i]) {
+
+                if (unsortedArray[j] < unsortedArray[i]) {  //Switch Elements of Array
                     int key = unsortedArray[i];
                     unsortedArray[i] = unsortedArray[j];
                     unsortedArray[j] = key;
