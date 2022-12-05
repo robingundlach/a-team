79289609f1d6ebf7c191fdf314516e0717e11f05
Robin Gundlach <gundlachrobin@gmail.com>
2022-12-05 14:37:51 +0100

    finito maximo

--- Aufgabe3/insertSort/src/Main.java
+++ Aufgabe3/insertSort/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] unsortedArray = {8, 7, 5, 3, 1, 9, 6, 4, 2};
+        System.out.println(Arrays.toString(unsortedArray));
+        System.out.println(Arrays.toString(selectionSort(unsortedArray)));
+    }
+    public static int[] selectionSort(int[] unsortedArray){
+        for (int i = 0; i< unsortedArray.length -1; i++){
+            for (int j = i+1; j < unsortedArray.length; j++) {
+                if (unsortedArray[j] < unsortedArray[i]) {
+                    int key = unsortedArray[i];
+                    unsortedArray[i] = unsortedArray[j];
+                    unsortedArray[j] = key;
+                }
+            }
+        }
+        return unsortedArray;
     }
 }
