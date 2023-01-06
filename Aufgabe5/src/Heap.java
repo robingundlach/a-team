b5da7e62937d35f4866cb65242a8d8279b925816
Robin Gundlach <gundlachrobin@gmail.com>
2023-01-06 16:43:50 +0100

    Main amk

--- Aufgabe5/src/Heap.java
+++ Aufgabe5/src/Heap.java
@@ -1,16 +1,6 @@
 import java.util.Arrays;
 
 class Heap {
-    public static void main(String[] args) {
-        int[] a = {1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17};
-
-        buildHeap(a);
-        System.out.println(Arrays.toString(a));
-
-        heapSort(a);
-        System.out.println(Arrays.toString(a));
-    }
-
     // Builds a heap from an array a
     public static void buildHeap(int[] a) {
         for (int i = a.length / 2 - 1; i >= 0; i--) {
